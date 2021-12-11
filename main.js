665a1b158b329644a03daee418b7e16f8cec0955
= <it7mdx@googlemail.com>
2021-12-11 21:16:47 +0300

    improve UI

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 import '@picocss/pico'
+import 'bootstrap/dist/css/bootstrap-grid.css'
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>stay tuned...</h1>
-  <section>
-  <article class="card">
-    <img src="./logo-01.svg" />  
-  </article>
-  </section>
-`
+/*document.querySelector('#app').innerHTML = `
+  
+`*/

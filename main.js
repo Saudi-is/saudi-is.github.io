119af813407e660ddcc1ef0add73c5925e71ba83
= <it7mdx@googlemail.com>
2021-12-11 08:21:06 +0300

    first init

---
+++ main.js
@@ -0,0 +1,11 @@
+import '@picocss/pico'
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>stay tuned...</h1>
+  <section>
+  <article class="card">
+    <img src="./logo-01.svg" />  
+  </article>
+  </section>
+`
